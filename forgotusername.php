7e0fe0782f3cb9b79418e83195b5b15dabc4fa46
PCMKUIT <145355564+PCMKUIT@users.noreply.github.com>
2024-11-18 01:35:35 +0700

    Fix 2 Bug: Stolen Token & Blind SQLi
    The Same Notification for Stolen Token and Prepared Statement for Blind SQL Injection

--- forgotusername.php
+++ forgotusername.php
@@ -1,5 +1,7 @@
 <?php
     session_start();
+
+    // Kiểm tra nếu người dùng đã đăng nhập
     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
         header('location: /index.php');
         die();
@@ -8,14 +10,35 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $username = $_POST['username'];
 
+        // Kết nối cơ sở dữ liệu
         include('includes/db_connect.php');
-        $ret = pg_query($db, "select * from users where username='".$username."';");
 
-        if (pg_num_rows($ret) === 1) {
+        // Sử dụng prepared statement để tránh SQL Injection
+        $stmt = pg_prepare($db, "get_user", "SELECT * FROM users WHERE username = $1");
+        $result = pg_execute($db, "get_user", array($username));
+
+        // Kiểm tra nếu người dùng tồn tại và gửi token reset qua email
+        if (pg_num_rows($result) === 1) {
+            // Tạo một token ngẫu nhiên và lưu trữ vào cơ sở dữ liệu
+            $token = bin2hex(random_bytes(16));  // Tạo token ngẫu nhiên dài 32 ký tự
+
+            // Giả sử hàm send_reset_email() sẽ gửi email cho người dùng với liên kết chứa token
+            send_reset_email($username, $token);
+
+            // Sau khi gửi email, thông báo cho người dùng một cách an toàn
             $success = true;
         } else {
+            // Không tiết lộ thông tin cụ thể về người dùng, chỉ thông báo chung
             $error = true;
         }
+    }
+
+    // Hàm gửi email với liên kết chứa token
+    function send_reset_email($username, $token) {
+        // Giả sử bạn có một hàm gửi email, bạn sẽ tạo liên kết đổi mật khẩu với token
+        $reset_link = "https://yourwebsite.com/resetpassword.php?token=" . urlencode($token);
+        // Mã gửi email, ví dụ:
+        mail($username, "Reset Your Password", "Click here to reset your password: " . $reset_link);
     }
 ?>
 
@@ -34,11 +57,15 @@
                 won't take you too long :(</p>
                 <input name="username" placeholder="Username"><br><br>
                 <input type="submit" value="Send Reset Token"> 
-                <?php if (isset($error)){echo "<span style='color:red'>User doesn't exist.</span>";}
-                else if (isset($success)){echo "<span style='color:green'>User exists!</span>";} ?>
+
+                <!-- Không tiết lộ người dùng có tồn tại hay không, chỉ thông báo chung -->
+                <?php if (isset($error) || isset($success)) {
+                    echo "<span style='color:blue'>If this username exists, a reset link will be sent to the associated email address.</span>";
+                } ?>
                 <br><br>
                 <?php include('includes/login_footer.php'); ?>
             </form>
         </div>
     </body>
 </html>
+
