1fc4b2ad4b7d0c28ec7289f4d6091b0554017c2f
PCMKUIT <145355564+PCMKUIT@users.noreply.github.com>
2024-11-18 01:28:31 +0700

    Database with Hash256+Salt-Credential

--- .docker/setup.sql
+++ .docker/setup.sql
@@ -1,13 +1,4 @@
-
--- Create a "setup_complete" table if it doesn't exist
-CREATE TABLE IF NOT EXISTS setup_complete (completed BOOLEAN);
-
--- Check if setup has already run
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM setup_complete WHERE completed = TRUE) THEN
-        -- Only run these commands if setup has not been completed
-		CREATE TABLE users (
+CREATE TABLE users (
 	uid SERIAL PRIMARY KEY NOT NULL,
 	username TEXT NOT NULL,
 	password TEXT NOT NULL,
@@ -36,24 +27,24 @@ CREATE TABLE motd_images (
 	title TEXT NOT NULL
 );
 
-        -- Insert statements
-        INSERT INTO users (username, password, description) VALUES
-        ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'BOSS'),
-        ('user1', '0a041b9462caa4a31bac3567e0b6e6fd9100787db2ab433d96f6d178cabfce90', 'Head of Security'),
-        ('user2', '6025d18fe48abd45168528f18a82e265dd98d421a7084aa09f61b341703901a3', 'Head of Management');
+INSERT INTO users 
+(username, password, description)
+VALUES
+('admin', '0188c451ed2f9b60b8ffed353b2fbee9208d543c04e26a6d61f2d2bdcc37cbf1', 'BOSS'),
+('user1', '5b09cfa5c2c8e6e468421251bfeb556c679d60ff37324065036bca54566c571d', 'Head of Security'),
+('user2', '0a8dc3467b9156a0347af4bcc5bbc837c1a5f64edb1d2a24792de08b8cf77298', 'Head of Management');
 
-        INSERT INTO class_posts (code, name, professor, ects, description) VALUES
-        ('187.B12', 'Denkweisen der Informatik', 'Purgathofer, Peter', 5.5, 'Very easy, but can be a bit frustrating'),
-        ('186.866', 'Algorithmen und Datenstrukturen', 'Kronegger, Martin', 8.0, 'Pretty hard, but very interesting'),
-        ('184.735', 'Einführung in die Künstliche Intelligenz', 'Eiter, Thomas', 3.0, 'Very lucky if you pass. Dont underestimate.'),
-        ('188.982', 'Privacy Enhancing Technologies', 'Weippl, Edgar', 3.0, 'Very fun, and easy to get a perfect grade. Takes a lot of time.');
+INSERT INTO class_posts
+(code, name, professor, ects, description)
+VALUES
+('187.B12', 'Denkweisen der Informatik', 'Purgathofer, Peter', 5.5, 'Very easy, but can be a bit frustrating'),
+('186.866', 'Algorithmen und Datenstrukturen', 'Kronegger, Martin', 8.0, 'Pretty hard, but very interesting'),
+('184.735', 'Einführung in die Künstliche Intelligenz', 'Eiter, Thomas', 3.0, 'Very lucky if you pass. Dont underestimate.'),
+('188.982', 'Privacy Enhancing Technologies ', 'Weippl, Edgar', 3.0, 'Very fun, and easy to get a perfect grade. Takes a lot of time.');
 
-        INSERT INTO motd_images (path, title) VALUES
-        ('images/motd_1.png','TU Library'),
-        ('images/motd_2.png','TU Hauptgebaude'),
-        ('images/motd_3.png','TU Freihaus');
-
-        -- Mark setup as completed
-        INSERT INTO setup_complete (completed) VALUES (TRUE);
-    END IF;
-END $$;
+INSERT INTO motd_images
+(path, title)
+VALUES
+('images/motd_1.png','TU Library'),
+('images/motd_2.png','TU Hauptgebaude'),
+('images/motd_3.png','TU Freihaus');
